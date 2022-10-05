13553a3e050ffb413d334f3828202ffef00505f5
Spencer Hayes <113189856+spencerschoolaccount@users.noreply.github.com>
2022-10-05 14:44:47 +0000

    re made everything

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+    <link rel="apple-touch-icon" sizes="180x180" href="/orb_favicon_io/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/orb_favicon_io/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/orb_favicon_io/favicon-16x16.png">
+    <link rel="manifest" href="/orb_favicon_io/site.webmanifest">
+    <title>FORM</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+  </head>
+  <body>
+    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+      <header class="mdl-layout__header">
+        <div class="mdl-layout__header-row">
+          <span class="mdl-layout-title">FORM</span>
+        </div>
+    </header>
+    <main class="mdl-layout__content">
+      <div class="page-content" style="padding-left: 10px">
+        <!-- Simple Textfield -->
+        <form action="#">
+          <div class="mdl-textfield mdl-js-textfield">
+            <input class="mdl-textfield__input" type="text" id="sample1">
+            <label class="mdl-textfield__label" for="sample1">Text...</label>
+          </div>
+        </form>
+        <!-- Accent-colored raised button with ripple -->
+        <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+          SUBMIT
+        </button>
+      </div>
+    </main>
+  </body>
+</html>
